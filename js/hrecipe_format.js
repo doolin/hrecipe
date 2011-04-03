65af95ae835875dcd3b16e4030edfc90ff84c0f0
Dave Doolin <david.doolin@gmail.com>
2011-04-02 20:18:20 -0700

    v0.5.4.5

---
+++ js/hrecipe_format.js
@@ -0,0 +1,321 @@
+
+/*global tb_show, hrecipe_handle, hrecipe_qttoolbar:true, document, newbutton:true, tb_remove, url */
+/*global tinyMCE, edInsertContent, edCanvas */
+
+
+/*
+ * enqueue this script with a dependency on 'quicktags'
+ */
+//(function () {
+
+  //alert("In script...");
+  // Get this script included...
+  //http://wordpress.org/support/topic/understanding-admin_print_scripts-page-hook?replies=13
+
+  var hrecipe_from_gui;
+
+  // TODO: rename these next two functions appropriately.
+  function edInsertHRecipe() {    
+    tb_show("Add an hRecipe", hrecipe_handle.PluginsUrl + "/view/lightbox.php?TB_iframe=true");
+    hrecipe_from_gui = true; /** Called from TinyMCE **/
+  }
+
+
+  function edInsertHRecipeCode() {
+    tb_show("Add an hRecipe", hrecipe_handle.PluginsUrl + "/view/lightbox.php?TB_iframe=true");
+    hrecipe_from_gui = false; /** Called from Quicktags **/
+  }
+
+  hrecipe_qttoolbar = document.getElementById("ed_toolbar");
+/*
+  if (hrecipe_qttoolbar === null) {
+    alert("ed_toolber element is null");
+  }
+ */
+  //if (hrecipe_qttoolbar = document.getElementById("ed_toolbar")) {
+  if (hrecipe_qttoolbar !== null) {
+    newbutton = document.createElement("input");
+    newbutton.type = "button";
+    newbutton.id = "ed_hrecipe";
+    newbutton.className = "ed_button";
+    newbutton.value = "hRecipe";
+    newbutton.onclick = edInsertHRecipeCode;
+    hrecipe_qttoolbar.appendChild(newbutton);
+  }
+
+  function edInsertHRecipeAbort() {
+    tb_remove();
+  } 
+
+  
+  function google_compliant_rating(itemRating) {
+
+    var markup = '';
+    //var solid_star = ''
+    //var outline_star = ''
+      
+    if (itemRating) {        
+      var i;
+      var itemRatingValue = parseInt(itemRating, 10);
+        
+      markup =  '<p class="review hreview-aggregate">';
+      markup += hrecipe_handle.hrecipe_rating_text + ' ';
+          
+      markup += '<span class="rating">';
+      markup += '<span class="average">' + itemRating + ' </span> ';
+      markup += hrecipe_handle.hrecipe_stars_text + ':&nbsp; ';
+        
+      // These are the loops the print the star symbols.  
+      var stars = 0;
+      for (i = 1; i <= itemRatingValue; i++) {          
+        stars++;
+        markup += '&#9733;'; // solid_star
+      }
+       
+      for (i = stars; i < 5; i++) {
+        markup += '&#9734;'; // outline_star
+      }
+        
+      markup += '<span class="count"> 1</span> review(s)';
+      markup += '</span>';  
+      markup += '</p>';
+
+    } // End if
+
+    return markup;
+      
+  } 
+
+
+
+  function format_ingredients(itemIngredients) {
+    //listtype
+    var lt =  (hrecipe_handle.hrecipe_ingredientlist === 'bullets') ? 'ul' : 'ol';
+    var imarkup = '';
+    var lines = '';
+    var i;
+    //lines = itemIngredients.split("\*");
+    lines = itemIngredients.split("*");
+    imarkup = '<div class="ingredients">';
+    imarkup += '<h4  class="ingredients">' + hrecipe_handle.hrecipe_ingredients_text + '</h4>';
+    imarkup += '<' + lt + ' class="ingredients">';
+    for (i = 0; i < lines.length; i++) {
+      // Crockford doesn't like continue, consider refactoring.
+      if (lines[i] === '') { continue; }
+      imarkup += '<li class="ingredient">' + lines[i] + '</li>';
+    }
+    imarkup += '</' + lt + '>';
+    imarkup += '</div>';
+    return imarkup;
+  }
+
+
+  function format_instructions(itemDescription) {
+    var imarkup = '';
+    var lines = '';
+    var i;
+    //lines = itemDescription.split("\*");
+    lines = itemDescription.split("*");
+    imarkup = '<div class="instructions">';
+    imarkup += '<h4 class="instructions">' + hrecipe_handle.hrecipe_instructions_text + '</h4>';
+    imarkup += '<ol class="instructions">';
+    for (i = 0; i < lines.length; i++) {
+      if (lines[i] === '') { continue; }
+      imarkup += '<li>' + lines[i] + '</li>';
+    }
+    imarkup += '</ol>';
+    imarkup += '</div>';
+    return imarkup;
+  }
+
+  function format_quicknotes(itemQuicknotes) {
+    var imarkup = '';
+    //var lines = '';
+    //lines = itemQuicknotes.split("\*");
+    imarkup = '<div class="quicknotes">';
+    imarkup += '<h4 class="quicknotes">' + hrecipe_handle.hrecipe_quicknotes_text  + '</h4>';
+    imarkup += '<p class="quicknotes">';
+    imarkup += itemQuicknotes;
+    imarkup += '</p>';
+    imarkup += '</div>';
+    return imarkup;
+  }
+
+
+  function format_variations(itemVariations) {
+    var imarkup = '';
+    //var lines = '';
+    //lines = itemVariations.split("\*");
+    imarkup = '<div class="variations">';
+    imarkup += '<h4>' + hrecipe_handle.hrecipe_variations_text + '</h4>';
+    //imarkup += '<h4>Variations</h4>';
+    imarkup += '<p class="variations">';
+    imarkup += itemVariations;
+    imarkup += '</p>';
+    imarkup += '</div>';
+    return imarkup;
+  }
+
+
+
+  function format_summary(itemSummary) {
+    var markup = '';
+    if (itemSummary === '') { return; }
+    markup = '<p class="summary">';
+    markup += '<strong>' +  hrecipe_handle.hrecipe_summary_text + '</strong>: ';
+    markup += '<em>' + itemSummary + '</em>';    
+    markup += '</p>';
+    return markup;
+  }
+
+
+/*
+  function format_summary(itemSummary) {
+    var markup = '';
+    if (itemSummary === '') { return; }
+    markup = '<h4 class="summary">';
+    markup += hrecipe_handle.hrecipe_summary_text;
+    markup += '<em>' + itemSummary + '</em>';    
+    markup += '</h4>';
+    return markup;
+  }
+*/
+
+/**
+ * Thanks for Michael Allen Smith for help 
+ * with bringing duration into spec so that 
+ * hrecipes will be properly displayed as 
+ * Google Rich Snippets.  Please visit Michael:
+ * http://criticalmas.com/
+ */
+ // Example from http://microformats.org/wiki/hrecipe#duration
+ //<span class="duration"><span class="value-title" title="PT1H30M"> </span>90 min</span>
+ // TODO: Add an hrlabel span for formatting
+  function format_duration(totalminutes) {
+
+    //Convert the minutes into hours and minutes
+    var hours = Math.floor(totalminutes / 60);
+    var minutes = totalminutes % 60;
+    
+    var markup = '';
+    markup = '<p class="duration">Cooking time (duration): ';
+    markup += '<span class="value-title" title="PT' + hours + 'H' + minutes + 'M"></span>';    
+    markup += totalminutes + '</p>';
+    return markup;
+  }
+    
+  
+/**
+ * Refactor format_item into something which can be used for all the 
+ * pieces of hrecipe output to reduce the maintenance load for 
+ * changing formatting etc.
+ */
+  
+  function format_item(hrclass, hrtext, hritem) {
+    var markup = '';
+    markup = '<p class="' + hrclass + '">';
+    markup += '<span class="hrlabel">' + hrtext + ': </span>';
+    markup += '<span class="hritem">' + hritem + '</span>';    
+    markup += '</p>';
+    return markup;
+  }
+
+
+  function format_enclosure(itemName, itemURL) {
+
+    var et =  (hrecipe_handle.hrecipe_enclosure === 'div') ? 'div' : 'fieldset';
+    var markup = '';
+  
+    if ("div" === et) {
+      markup += '<div class="hrecipe">';
+      markup += '<h2 class="fn">' + hrecipe_handle.hrecipe_recipe_text  + ': ';
+      markup += (itemURL ? '<a class="url" href="' + itemURL + '">' : '') + itemName + (itemURL ? '</a>' : '');
+      markup += '</h2>';
+    } else {
+      markup += '<fieldset class="hrecipe">';
+      markup += '<legend class="fn">' + hrecipe_handle.hrecipe_recipe_text  + ': ';
+      markup += (itemURL ? '<a class="url" href="' + itemURL + '">' : '') + itemName + (itemURL ? '</a>' : '');
+      markup += '</legend>';
+    } 
+  
+    return markup;
+  }
+
+  function linklove() {   
+    return 'Microformatting by <a href="http://website-in-a-weekend.net/hrecipe/" target="_blank">hRecipe</a>.<br />';
+  }
+
+  function reciply() {
+    return '<div class="reciply-addtobasket-widget" href="' + url + '"></div>';
+  }
+  
+// Add Restrictions, Yield, Author, and Published (date) into first parameter, 
+// an object which gets passed in this function.
+  function edInsertHRecipeDone(r) {
+    
+    tb_remove();
+  
+    var HRecipeOutput = ''; 
+    HRecipeOutput += format_enclosure(r.name, r.url);
+
+    HRecipeOutput += (r.summary     ? format_summary(r.summary) : '');
+    HRecipeOutput += (r.ingredients ? format_ingredients(r.ingredients) : '');
+    HRecipeOutput += (r.description ? format_instructions(r.description) : '');
+    HRecipeOutput += (r.quicknotes  ? format_quicknotes(r.quicknotes) : '');
+    HRecipeOutput += (r.variations  ? format_variations(r.variations) : '');
+    HRecipeOutput += (r.duration    ? format_duration(r.duration) : '');
+    
+    HRecipeOutput += (r.diettype    ? format_item('diettype', 'Diet type', r.diettype) : '');
+    HRecipeOutput += (r.dietother   ? format_item('dietother', 'Diet (other)', r.dietother) : '');
+    HRecipeOutput += (r.restriction ? format_item('restriction', 'Dietary restriction', r.restriction) : '');
+    HRecipeOutput += (r.servings    ? format_item('yield', 'Number of servings (yield)', r.servings) : '');
+    HRecipeOutput += (r.mealtype    ? format_item('mealtype', 'Meal type', r.mealtype) : '');
+    HRecipeOutput += (r.tradition   ? format_item('tradition', 'Culinary tradition', r.tradition) : '');
+
+
+    HRecipeOutput += (r.rating      ? google_compliant_rating(r.rating) : '');
+
+
+    var want_copyright = hrecipe_handle.hrecipe_copyright;
+    //alert (want_copyright);
+    if (want_copyright !== '') {
+      HRecipeOutput += 'Copyright &copy; ' + want_copyright + '.<br />';
+    }
+
+    ///*
+    var want_byline = hrecipe_handle.hrecipe_byline; 
+    if (want_byline !== '') {
+      HRecipeOutput += 'Recipe by ' + want_byline + '.<br />';
+    }
+    //*/
+
+    var want_linklove = (hrecipe_handle.hrecipe_linklove === 'on') ? 'true' : '';
+    //alert (want_linklove);
+    if (want_linklove) {
+      HRecipeOutput += linklove();
+    }
+  
+    var want_reciply = (hrecipe_handle.hrecipe_reciply === 'on') ? 'true' : '';
+    //alert (want_reciply);
+    if (want_reciply) {
+        // This is supposed to result from a function call 
+        // to reciply(), which for some reason is crashing the 
+        // entire hrecipe output.  Stupid.    
+      HRecipeOutput += '<div class="reciply-addtobasket-widget" href="' 
+        + hrecipe_handle.Permalink 
+        + '"></div>';
+    }
+  
+    var et =  (hrecipe_handle.hrecipe_enclosure === 'div') ? 'div' : 'fieldset';
+    HRecipeOutput += '</' + et + '>';
+    //HRecipeOutput += '</div>';
+
+    if (hrecipe_from_gui) {
+      tinyMCE.execInstanceCommand('content', 'mceInsertContent', false, HRecipeOutput);
+      tinyMCE.execCommand('mceCleanup');
+    } else {
+      edInsertContent(edCanvas, HRecipeOutput);
+    }
+  } // End edInsertHRecipeDone()
+
+//})();
---
+++ view/hrecipe_form_body.php
@@ -0,0 +1,226 @@
+<div class="wrap">
+<h2><?php _e('New Recipe','hrecipe'); ?></h2>
+
+
+<div>
+  
+  <div id="hrecipe-tab-wrapper">
+    <ul class="tabs">
+      <li><a href="#tab-1">General</a></li>
+      <li><a href="#tab-2">Ingredients/Instructions</a></li>
+      <li><a href="#tab-3">Notes/Variations</a></li>
+      <li><a href="#tab-4">Additional Information</a></li>
+    </ul>
+  </div><!-- tabbed menu wrapper -->
+
+<form name="recipeForm">
+
+<div id="tab-1" class="tab_content">
+
+<table class="form-table">
+
+<tr valign="top">
+<th scope="row"><?php _e('Name of recipe','hrecipe'); ?> </th>
+<td><input type="text" id="item-name" size="45" /></td>
+</tr>
+<tr valign="top">
+<th scope="row"><?php _e('Reference URL','hrecipe'); ?></th>
+<td><input type="text" id="item-url" size="45" /></td>
+</tr>
+
+<tr valign="top">
+<th scope="row"><?php _e('Summary','hrecipe'); ?></th>
+<td><input type="text" id="item-summary" size="45" /></td>
+</tr>
+</table>
+
+<ul class="tabs" style="margin-top:50px; border:0; background:transparent;">
+    <li class="btmtabs"><a href="#tab-2">Next</a></li>
+  <li class="btmtabs" style="float:right; display:inline;"><a href="#" onclick="javascript:submitForm()">Insert</a></li>
+  </ul>
+</div>
+
+<div id="tab-2" class="tab_content">
+
+<table class="form-table">
+<tr valign="top">
+<th scope="row"><?php _e('Ingredients','hrecipe'); ?><br /><?php _e('(May use HTML)','hrecipe'); ?></th>
+<td><textarea id="item-ingredients" rows="10" cols="45"></textarea></td>
+</tr>
+
+
+<tr valign="top">
+<th scope="row"><?php _e('Instructions','hrecipe'); ?><br /><?php _e('(May use HTML)','hrecipe'); ?></th>
+<td><textarea id="item-description" rows="10" cols="45"></textarea></td>
+</tr>
+</table>
+<ul class="tabs" style="margin-top:50px; border:0; background:transparent;">
+<li class="btmtabs"><a href="#tab-1">Back</a></li>
+    <li class="btmtabs"><a href="#tab-3">Next</a></li>
+  <li class="btmtabs" style="float:right; display:inline;"><a href="#" onclick="javascript:submitForm()">Insert</a></li>
+  </ul>
+</div>
+
+<div id="tab-3" class="tab_content">
+
+<table class="form-table">
+
+<tr valign="top">
+<th scope="row"><?php _e('Quick Notes','hrecipe'); ?><br /><?php _e('(May use HTML)','hrecipe'); ?></th>
+<td><textarea id="item-quicknotes" rows="5" cols="45"></textarea></td>
+</tr>
+
+
+<tr valign="top">
+<th scope="row"><?php _e('Variations','hrecipe'); ?><br /><?php _e('(May use HTML)','hrecipe'); ?></th>
+<td><textarea id="item-variations" rows="5" cols="45"></textarea></td>
+</tr>
+
+</table>
+<ul class="tabs" style="margin-top:50px; border:0; background:transparent;">
+<li class="btmtabs"><a href="#tab-2">Back</a></li>
+    <li class="btmtabs"><a href="#tab-4">Next</a></li>
+  <li class="btmtabs" style="float:right; display:inline;"><a href="#" onclick="javascript:submitForm()">Insert</a></li>
+  </ul>
+</div>
+
+<div id="tab-4" class="tab_content">
+
+<table class="form-table">
+
+<tr valign="top">
+<th scope="row"><?php _e('Preparation time (minutes)','hrecipe'); ?></th>
+<td><input type="text" id="item-duration" size="5" /></td>
+</tr>
+
+<tr valign="top">
+<th scope="row"><?php _e('Recipe type:','hrecipe'); ?></th>
+<td>
+<select id="item-mealtype">
+<option></option>
+<option><?php _e('breakfast','hrecipe'); ?></option>
+<option><?php _e('lunch','hrecipe'); ?></option>
+<option><?php _e('dinner','hrecipe'); ?></option>
+<option><?php _e('supper','hrecipe'); ?></option>
+<option><?php _e('brunch','hrecipe'); ?></option>
+<option><?php _e('dessert','hrecipe'); ?></option>
+<option><?php _e('cocktail','hrecipe'); ?></option>
+<option><?php _e('hors d\'oerves','hrecipe'); ?></option>
+<option><?php _e('snack','hrecipe'); ?></option>
+</select>
+</td>
+</tr>
+
+
+<tr valign="top">
+<th scope="row"><?php _e('Number of servings:','hrecipe'); ?></th>
+<td>
+<select id="item-servings">
+<option></option>
+<option>1</option>
+<option>2</option>
+<option>3</option>
+<option>4</option>
+<option>6</option>
+<option>8</option>
+<option>12</option>
+</select>
+</td>
+</tr>
+
+
+<tr valign="top">
+<th scope="row"><?php _e('Diet type:','hrecipe'); ?></th>
+<td>
+<select id="item-diettype">
+<option></option>
+<option><?php _e('Vegetarian','hrecipe'); ?></option>
+<option><?php _e('Pescatarian','hrecipe'); ?></option>
+<option><?php _e('Vegan','hrecipe'); ?></option>
+<option><?php _e('Macrobiotic','hrecipe'); ?></option>
+</select>
+</td>
+</tr>
+
+<tr valign="top">
+<th scope="row"><?php _e('Diet restriction:','hrecipe'); ?></th>
+<td>
+<select id="item-dietrestriction">
+<option></option>
+<option><?php _e('Kosher','hrecipe'); ?></option>
+<option><?php _e('Halal','hrecipe'); ?></option>
+<option><?php _e('Buddhist','hrecipe'); ?></option>
+</select>
+</td>
+</tr>
+
+
+<tr valign="top">
+  <th scope="row"><?php _e('Diet (other):','hrecipe'); ?><br />
+  <?php _e('(Check all that apply)','hrecipe'); ?></th>
+<td>
+<input type="checkbox" id="low_calorie" name="item-dietother" value="Low calorie"><?php _e('Low calorie','hrecipe'); ?>&nbsp;&nbsp;  
+<input type="checkbox" id="reduced_fat" name="item-dietother" value="Reduced fat"><?php _e('Reduced fat','hrecipe'); ?>&nbsp;&nbsp; 
+<input type="checkbox" id="reduced_carbohydrate" name="item-dietother" value="Reduced carbohydrate"><?php _e('Reduced carbohydrate','hrecipe'); ?><br /> 
+<input type="checkbox" id="high_protein" name="item-dietother" value="High protein"><?php _e('High protein','hrecipe'); ?>&nbsp;&nbsp; 
+<input type="checkbox" id="gluten_free" name="item-dietother" value="Gluten free"><?php _e('Gluten free','hrecipe'); ?>&nbsp;&nbsp; 
+<input type="checkbox" id="raw" name="item-dietother" value="Raw"><?php _e('Raw','hrecipe'); ?>
+</td>
+</tr>
+
+
+
+<tr valign="top">
+<th scope="row"><?php _e('Culinary Tradition:','hrecipe'); ?></th>
+<td>
+<select id="item-culinarytradition">
+<option></option>
+<option><?php _e('USA (General)','hrecipe'); ?></option>
+<option><?php _e('USA (Traditional)','hrecipe'); ?></option>
+<option><?php _e('USA (Southern)','hrecipe'); ?></option>
+<option><?php _e('USA (Southwestern)','hrecipe'); ?></option>
+<option><?php _e('USA (Nouveau)','hrecipe'); ?></option>
+<option><?php _e('English','hrecipe'); ?></option>
+<option><?php _e('Irish','hrecipe'); ?></option>
+<option><?php _e('French','hrecipe'); ?></option>
+<option><?php _e('German','hrecipe'); ?></option>
+<option><?php _e('Spanish','hrecipe'); ?></option>
+<option><?php _e('Chinese','hrecipe'); ?></option>
+<option><?php _e('Thai','hrecipe'); ?></option>
+<option><?php _e('Mexican','hrecipe'); ?></option>
+<option><?php _e('Central American','hrecipe'); ?></option>
+<option><?php _e('Russian','hrecipe'); ?></option>
+<option><?php _e('Scandinavian','hrecipe'); ?></option>
+<option><?php _e('Italian','hrecipe'); ?></option>
+<option><?php _e('Persian','hrecipe'); ?></option>
+<option><?php _e('Indian (Southern)','hrecipe'); ?></option>
+<option><?php _e('Indian (Northern)','hrecipe'); ?></option>
+<option><?php _e('Pakistani','hrecipe'); ?></option>
+<option><?php _e('African','hrecipe'); ?></option>
+<option><?php _e('Middle Eastern','hrecipe'); ?></option>
+<option><?php _e('Greek','hrecipe'); ?></option>
+<option><?php _e('Vietnamese','hrecipe'); ?></option>
+<option><?php _e('Indian (Marathi Cuisine)','hrecipe'); ?></option></select></td>
+</tr>
+
+<tr valign="top">
+<th scope="row"><?php _e('My rating:','hrecipe'); ?><br />
+<?php _e('(number of stars, leave blank if rating is undesired)','hrecipe'); ?></th>
+<td>
+<select id="item-rating">
+<option></option>
+<option>1</option>
+<option>2</option>
+<option>3</option>
+<option>4</option>
+<option>5</option>
+</select></td>
+</tr>
+</table>
+<ul class="tabs" style="margin-top:50px; border:0; background:transparent;">
+<li class="btmtabs"><a href="#tab-3">Back</a></li>
+  <li class="btmtabs" style="float:right; display:inline;"><a href="#" onclick="javascript:submitForm()">Insert</a></li>
+  </ul>
+</div>
+</form>
+</div>
