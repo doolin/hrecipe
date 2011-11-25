c7f45a544f1e678251da6a0479becc91b218db90
Dave Doolin <david.doolin@gmail.com>
2011-11-24 16:50:40 -0800

    updated quicktags to work with 3.3

--- js/hrecipe_launch.js
+++ js/hrecipe_launch.js
@@ -3,31 +3,34 @@
 
   var hrecipe_from_gui;
 
-  // TODO: rename these next two functions appropriately.
   function edInsertHRecipe() {
     tb_show("Add an hRecipe", 'media-upload.php?type=hrecipe&tab=hrecipe&amp;TB_iframe=true');
     hrecipe_from_gui = true; // Called from TinyMCE
   }
 
-
-  function edInsertHRecipeCode() {
+  function add_hrecipe_quicktag() {
     tb_show("Add an hRecipe", 'media-upload.php?type=hrecipe&tab=hrecipe&amp;TB_iframe=true');
     hrecipe_from_gui = false; // Called from Quicktags
   }
 
-  hrecipe_qttoolbar = document.getElementById("ed_toolbar");
-
-
-  if (hrecipe_qttoolbar !== null) {
+  function hrecipe_make_quicktag() {
     newbutton = document.createElement("input");
     newbutton.type = "button";
     newbutton.id = "ed_hrecipe";
     newbutton.className = "ed_button";
     newbutton.value = "hRecipe";
-    newbutton.onclick = edInsertHRecipeCode;
+    newbutton.onclick = add_hrecipe_quicktag;
     hrecipe_qttoolbar.appendChild(newbutton);
+  }
+
+  hrecipe_qttoolbar = document.getElementById("ed_toolbar");
+
+  if (hrecipe_qttoolbar !== null) {
+    hrecipe_make_quicktag();
+  } else {
+    QTags.addButton('qt_hrecipe', 'hRecipe', add_hrecipe_quicktag);
   }
 
   function edInsertHRecipeAbort() {
     tb_remove();
-  } 
+  }
